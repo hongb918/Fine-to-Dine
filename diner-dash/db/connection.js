51b05ceabe3b78eb4d7f6384fce3ff627e922764
hongb918 <hong.b918@gmail.com>
2022-04-27 18:38:38 -0500

    deploy 2

--- diner-dash/db/connection.js
+++ diner-dash/db/connection.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose')
 const mongoURI =
 process.env.NODE_ENV === 'production'
         ? process.env.DB_URL
-        // : process.env.DEV_DB_URL
-        : "mongodb+srv://hongbui:bareFOOT@cluster1.fcv6b.mongodb.net/project2?retryWrites=true&w=majority"
+        : process.env.DEV_DB_URL
+        // : "mongodb+srv://hongbui:bareFOOT@cluster1.fcv6b.mongodb.net/project2?retryWrites=true&w=majority"
 
 mongoose.connect(mongoURI)
     .then(() => console.log(`Connected to db`))
